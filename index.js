706d276861eded230484e28127748a2984a97a08
koyeary <katyeary@gmail.com>
2020-07-23 12:38:46 -0700

    adjustments to improve responsiveness. Added skills modal to html and js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.modal').modal();
+
+/*     $(window).scroll(function() {
+      $('.hideme').each(function(i){
+
+        const objBottom = $(this).position().top + $(this).outerHeight();
+        const windowBottom = $(window).scrollTop() + $(window).height();
+
+        if (windowBottom > objBottom) {
+          $this.animate({'opacity':'1'}, 500);
+        }
+        
+      });
+    }); */
   });
+
+  const checkpoint = 300;
+
