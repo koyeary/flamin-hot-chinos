1cf72be88c448328e3ac8511ab76bd8db21a6c19
koyeary <katyeary@gmail.com>
2020-07-24 12:18:38 -0700

    create models dir with Users model

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ require("./routes/api-routes.js")(app);
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    console.log(`==> 🌎  Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`);
   });
 });
