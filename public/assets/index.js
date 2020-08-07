6f71316c0d44cb68b65c4e14c5257ecf06dd777b
koyeary <katyeary@gmail.com>
2020-08-07 11:46:38 -0700

    Style login modals

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,3 +1,10 @@
 
+$("#Trigger").click(function () {
+    $("#Slider").toggleClass("slidedown slideup");
+      if ($("#Slider").hasClass("slideup"))
+        $("#Slider").removeClass("slideup").addClass("slidedown");
+      else
+          $("#Slider").removeClass("slidedown").addClass("slideup");
+  });
 
 
