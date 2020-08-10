de92e0261a2a52f9859f846266181c83ba0efdfe
koyeary <katyeary@gmail.com>
2020-08-09 20:11:47 -0700

    parallax

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
-$(window).load(function() {
-    $("img").fadeIn("slow", function() {
-        //animation complete
-    })
-})
-
+    $(function() {
+        $(window).scroll(function() {
+          const winTop = $(window).scrollTop();
+          if (winTop >= 30) {
+            $("body").addClass("sticky-shrinknav-wrapper");
+          } else{
+            $("body").removeClass("sticky-shrinknav-wrapper");
+          }
+        });
+      });
+      
+      
 }); 
